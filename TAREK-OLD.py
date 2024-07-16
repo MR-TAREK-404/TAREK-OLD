3716d69c0f8d44282d8aee45dd83205824f58021
TAREK <tarek713609@gmail.com>
2024-07-16 19:40:45 +0600

    Create TAREK-OLD.py

---
+++ TAREK-OLD.py
@@ -0,0 +1,11 @@
+#খানকীর পোলা ফাইল পারলে চুরি করে দেখা 
+#তোর আব্বু তারেক এর ফাইল নিবি
+#I AM FUCK YOU
+import platform,os,time
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import public_run_64
+elif bit == '32bit':
+    import public_run_32
+    #while True:
+        #print("Now 32 bit is off please wait")
